3f78566d66706ba45cdec979e3f30838261aab4a
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-06-13 19:15:17 -0400

    refactor: replace if/throw blocks with expect function for greet and congratulate outputs

--- exercises/02.test-structure/01.problem.assertions/greet.ts
+++ exercises/02.test-structure/01.problem.assertions/greet.ts
@@ -6,34 +6,15 @@ function congratulate(name: string) {
 	return `Congrats, ${name}!`
 }
 
-let message = greet('John')
+expect(greet('John')).toBe('Hello, John!')
+expect(congratulate('Sarah')).toBe('Congrats, Sarah!')
 
-// 🐨 Replace these if/throw blocks with the "expect" function
-// you will introduce later down this file.
-// 💰 expect(greet('John')).toBe('Hello, John!')
-if (message !== 'Hello, John!') {
-	throw new Error(
-		`Expected message to equal to "Hello, John!" but got "${message}"`,
-	)
+function expect(actual: unknown) {
+	return {
+		toBe(expected: unknown) {
+			if (actual !== expected) {
+				throw new Error(`Expected ${actual} to equal to ${expected}`)
+			}
+		},
+	}
 }
-
-message = congratulate('Sarah')
-if (message !== 'Congrats, Sarah!') {
-	throw new Error(
-		`Expected message to equal to "Congrats, Sarah!" but got "${message}"`,
-	)
-}
-
-// 🐨 Create a function called "expect" that accepts a single argument:
-// the "actual" value.
-
-// 🐨 From the "expect" function, return an object that has
-// a "toBe" property, which equals to a new function.
-// The "toBe" function accepts the "expected" argument.
-// 💰 Use shorthand function declaration on objects:
-// return { toBe(expected) {} }
-
-// 🐨 In the "toBe" function, compare the "actual" and the "expected"
-// values, and throw an error if they don't mtach.
-// 💰 You can throw an error like this one:
-// new Error(`Expected ${actual} to equal to ${expected}`)
