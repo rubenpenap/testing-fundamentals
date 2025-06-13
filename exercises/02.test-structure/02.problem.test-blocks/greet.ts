0ce4154b225d7b821dfe02ab7076d4060dde5453
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-06-13 19:28:51 -0400

    test: wrap existing expect calls in test function and implement test structure for greet and congratulate functions

--- exercises/02.test-structure/02.problem.test-blocks/greet.ts
+++ exercises/02.test-structure/02.problem.test-blocks/greet.ts
@@ -1,25 +1,18 @@
 function greet(name: string) {
-	return 'Hello, ${name}!'
+	return `Hello, ${name}!`
 }
 
 function congratulate(name: string) {
 	return `Congrats, ${name}!`
 }
 
-// 🐨 Wrap each of the existing "expect()" calls in the
-// "test()" function you will write later down this file.
+test('returns a greeting message for the given name', () => {
+	expect(greet('John')).toBe('Hello, John!')
+})
 
-// 💰 Use meaningful test case names, like:
-// - returns a greeting message for the given name
-// - returns a congratulation message for the given name
-
-// 💰 Provide the callback to the "test()" function that
-// will run these assertions.
-// test('', () => {
-//  expect(greet('John')).toBe('Hello, John!')
-// })
-expect(greet('John')).toBe('Hello, John!')
-expect(congratulate('Sarah')).toBe('Congrats, Sarah!')
+test('returns a congratulation message for the given name', () => {
+	expect(congratulate('Sarah')).toBe('Congrats, Sarah!')
+})
 
 function expect(actual: unknown) {
 	return {
@@ -31,16 +24,12 @@ function expect(actual: unknown) {
 	}
 }
 
-// 🐨 Create a new function called "test".
-// The "test" function accepts two arguments: "title" and "callback".
-
-// 🐨 In the "test" function, call the "callback" function
-// and print a message to the console if it completes without throwing.
-// Include a checkbox icon and the test's title in the message.
-// 💰 See if you can *try* and *catch* any errors from the "callback".
-// 💰 Use this template for success messages: `✓ ${title}`
-
-// 🐨 If the calling the "callback" function throws an error,
-// print another message to the console. In this one, include a cross
-// icon, the test's name, and the thrown error.
-// 💰 Use this template for error messages: `✗ ${title}`
+function test(title: string, callback: Function) {
+	try {
+		callback()
+		console.log(`✓ ${title}`)
+	} catch (error) {
+		console.log(`✗ ${title}`)
+		console.log(error)
+	}
+}
