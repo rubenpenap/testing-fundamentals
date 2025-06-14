896ecff368c2d355effecbcf0dc6a327dd9eaffa
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-06-13 20:46:33 -0400

    test: enhance date mocking in tests using Proxy for fixed date management

--- exercises/02.test-structure/04.problem.hooks/greet.test.ts
+++ exercises/02.test-structure/04.problem.hooks/greet.test.ts
@@ -1,28 +1,17 @@
 import { greet, congratulate } from './greet.js'
 
-// 🐨 In the test, we need to mock the date returned
-// from the "Date" constructor. First, store the original
-// implementation of the "globalThis.Date" constructor
-// 💰 const OriginalDate = globalThis.Date
+const OriginalDate = globalThis.Date
 
-// 🐨 Next, call the global "beforeAll" function, passing it
-// an empty arrow function as the "callback" argument.
-// 💰 beforeAll(() => {})
+beforeAll(() => {
+	globalThis.Date = new Proxy(OriginalDate, {
+		construct: (target, args: ConstructorParameters<typeof Date>) =>
+			args.length ? new target(...args) : new target(2024, 0, 1),
+	})
+})
 
-// 🐨 In the callback to "beforeAll", reassign the global "Date"
-// constructor to a Proxy that returns a fixed date.
-// 💰 globalThis.Date = new Proxy(globalThis.Date, {
-//	construct: () => new OriginalDate('2024-01-01'),
-// })
-
-// 🐨 Call the global "afterAll" function, passing it
-// an empty arrow function as the "callback" argument.
-// 💰 afterAll(() => {})
-
-// 🐨 In the callback to "afterAll", restore the global "Date"
-// class to its original implementation.
-// 💰 globalThis.Date = OriginalDate
-
+afterAll(() => {
+	globalThis.Date = OriginalDate
+})
 test('returns a greeting message for the given name', () => {
 	expect(greet('John')).toBe('Hello, John! Happy, Monday.')
 })
